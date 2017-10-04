f54224cbc8d03d03e6533ec0989735f1c22331c7
KotaroKishie <32289056+KotaroKishie@users.noreply.github.com>
2017-10-04 16:57:13 +0900

    Ebi　追加

---
+++ Assets/Resources/Kishie/Ebi/Scripts/GameStart_scr_Ebi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class GameStart_scr_Ebi : MonoBehaviour {
+
+	public GameObject GameStart_Ebi;
+	int t = 0;
+	void Start () {
+
+	}
+
+	void Update () {
+		t++;
+		if(t>=60){
+			GameStart_Ebi.SetActive (false);
+		}
+
+	}
+}
---
+++ Assets/Resources/Kishie/Ebi/Scripts/player_scr.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class player_scr: MonoBehaviour {
+	public Vector2 SPEED = new Vector2(0.1f, 0.2f);
+	bool move = true;
+	private GameObject came = null;
+	private Camera cs;
+	private float defaultCameraScale = 10f;
+	bool zoom = false;
+
+	void Start () {
+		//GeikodoManager.isSuccess = false;
+		came = GameObject.Find ("Main Camera");
+		cs = came.GetComponent<Camera> ();
+		cs.orthographicSize = defaultCameraScale;
+	}
+		
+	void Update () {
+		if (move) {
+			Move ();
+		}
+		if(zoom){
+			ZoomIn ();
+		}
+	}
+		
+
+	void OnTriggerEnter2D (Collider2D collision){
+		move = false;
+		zoom = true;
+		Invoke ("Death",2f);
+	}
+			
+
+	public void Move(){
+		Vector2 Position = transform.position;
+		Position.y -= SPEED.y;
+		if(Input.GetKey("left")){
+			Position.x -= SPEED.x;
+		} else if(Input.GetKey("right")){
+			Position.x += SPEED.x;
+		}
+		transform.position = Position;
+	}
+
+
+	public void Death (){
+		
+		//Destroy (gameObject);
+	}
+
+
+	public void ZoomIn(){
+		Debug.Log ("GameOver");
+
+		cs.orthographicSize -= 0.2f;
+//		if (cs.orthographicSize <= 2.0f) {
+//			cs.orthographicSize = 2.0f;
+//		}
+	}
+}
