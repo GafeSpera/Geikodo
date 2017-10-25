8e23bf2a0ef59bb9f071dbc2150a74510d1be079
kamotohashi <32289149+kamotohashi@users.noreply.github.com>
2017-10-26 02:44:07 +0900

    compreteDESIGN

--- Assets/Resources/Motohashi/DESIGN/script/StopD.cs
+++ Assets/Resources/Motohashi/DESIGN/script/StopD.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class StopD : MonoBehaviour {
 
+	public static bool Stoped;
+
 	// Use this for initialization
 	void Start () {
-		
+		Stoped = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.A)) {
 			RollingD.Roll = 0;
+			Stoped = true;
 		}
 	}
 }
