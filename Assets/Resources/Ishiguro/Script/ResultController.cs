3d128901b0e5f74f76856db801c05bab26953482
Ryo ishiguro <50fak227plas65tr@kch.biglobe.ne.jp>
2017-10-26 15:53:34 +0900

    リザルト作った

---
+++ Assets/Resources/Ishiguro/Script/ResultController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultController : MonoBehaviour {
+
+	public Sprite[] grades = new Sprite[5];
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Image> ().sprite = grades [GeikodoManager.score/2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
